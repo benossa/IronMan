398403603350e39cf27337503131badbe3c53562
Benjamin <benjamin.djonko@edu.fit.ba>
2018-03-31 21:40:13 +0200

    Prvi dizajn forme i osnovno podešavanje serijskog porta, čitanje podataka sa porta.

---
+++ IronMan/Main.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace IronMan
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbSerialPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPortStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSerialMonitor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbBaudRate = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnOpenSerial = new System.Windows.Forms.Button();
+            this.btnCloseSerial = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbSerialPort
+            // 
+            this.cbSerialPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSerialPort.FormattingEnabled = true;
+            this.cbSerialPort.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9"});
+            this.cbSerialPort.Location = new System.Drawing.Point(12, 30);
+            this.cbSerialPort.Name = "cbSerialPort";
+            this.cbSerialPort.Size = new System.Drawing.Size(177, 21);
+            this.cbSerialPort.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // lblPortStatus
+            // 
+            this.lblPortStatus.AutoSize = true;
+            this.lblPortStatus.Location = new System.Drawing.Point(145, 9);
+            this.lblPortStatus.Name = "lblPortStatus";
+            this.lblPortStatus.Size = new System.Drawing.Size(39, 13);
+            this.lblPortStatus.TabIndex = 2;
+            this.lblPortStatus.Text = "Closed";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Port Status:";
+            // 
+            // tbSerialMonitor
+            // 
+            this.tbSerialMonitor.Location = new System.Drawing.Point(195, 30);
+            this.tbSerialMonitor.Multiline = true;
+            this.tbSerialMonitor.Name = "tbSerialMonitor";
+            this.tbSerialMonitor.Size = new System.Drawing.Size(357, 236);
+            this.tbSerialMonitor.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(342, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Serial Monitor";
+            // 
+            // cbBaudRate
+            // 
+            this.cbBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBaudRate.FormattingEnabled = true;
+            this.cbBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.cbBaudRate.Location = new System.Drawing.Point(12, 76);
+            this.cbBaudRate.Name = "cbBaudRate";
+            this.cbBaudRate.Size = new System.Drawing.Size(177, 21);
+            this.cbBaudRate.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Baud Rate";
+            // 
+            // btnOpenSerial
+            // 
+            this.btnOpenSerial.Location = new System.Drawing.Point(12, 214);
+            this.btnOpenSerial.Name = "btnOpenSerial";
+            this.btnOpenSerial.Size = new System.Drawing.Size(177, 23);
+            this.btnOpenSerial.TabIndex = 8;
+            this.btnOpenSerial.Text = "Open Serial";
+            this.btnOpenSerial.UseVisualStyleBackColor = true;
+            this.btnOpenSerial.Click += new System.EventHandler(this.btnOpenSerial_Click);
+            // 
+            // btnCloseSerial
+            // 
+            this.btnCloseSerial.Location = new System.Drawing.Point(12, 243);
+            this.btnCloseSerial.Name = "btnCloseSerial";
+            this.btnCloseSerial.Size = new System.Drawing.Size(177, 23);
+            this.btnCloseSerial.TabIndex = 9;
+            this.btnCloseSerial.Text = "Close Serial";
+            this.btnCloseSerial.UseVisualStyleBackColor = true;
+            this.btnCloseSerial.Click += new System.EventHandler(this.btnCloseSerial_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 278);
+            this.Controls.Add(this.btnCloseSerial);
+            this.Controls.Add(this.btnOpenSerial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbBaudRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbSerialMonitor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPortStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbSerialPort);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbSerialPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPortStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbSerialMonitor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbBaudRate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnOpenSerial;
+        private System.Windows.Forms.Button btnCloseSerial;
+    }
+}
---
+++ IronMan/Main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IronMan
+{
+
+    public partial class Main : Form
+    {
+        private SerialPort serial { get; set; }
+        private System.Windows.Forms.Timer timmer { get; set; }
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            timmer = new System.Windows.Forms.Timer();
+            serial = new SerialPort();
+            cbSerialPort.SelectedIndex = 2;
+            cbBaudRate.SelectedIndex = 0;
+
+            timmer.Enabled = true;
+            timmer.Interval = 1000;
+            timmer.Tick += new EventHandler(timer_Tick);
+            timmer.Start();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serial.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (serial.IsOpen)
+                tbSerialMonitor.Text += serial.ReadLine() + Environment.NewLine;
+        }
+
+        private void btnOpenSerial_Click(object sender, EventArgs e)
+        {
+            if (!serial.IsOpen)
+            {
+                serial.BaudRate = int.Parse(cbBaudRate.Text);
+                serial.PortName = cbSerialPort.Text;
+                serial.Open();
+            }
+            lblPortStatus.Text = serial.IsOpen ? "Open" : "Closed";
+        }
+
+        private void btnCloseSerial_Click(object sender, EventArgs e)
+        {
+            if (serial.IsOpen)
+                serial.Close();
+            lblPortStatus.Text = serial.IsOpen ? "Open" : "Closed";
+        }
+    }
+}
